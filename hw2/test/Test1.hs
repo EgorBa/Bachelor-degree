d3d05afac30e19957d447e63a357484e72b28050
Bazhenov Egor <tujh.bazhenov.kbn00@mail.ru>
2020-12-15 18:56:13 +0300

    add property

--- hw2/test/Test1.hs
+++ hw2/test/Test1.hs
@@ -2,11 +2,23 @@ module Test1 where
 
 import Test.Tasty (TestTree)
 import Test.Tasty.Hspec (Spec, describe, it, shouldBe, shouldSatisfy, testSpec)
+import Test.Hspec.QuickCheck
 
 import Task1
 
 hspecTest1 :: IO TestTree
 hspecTest1 = testSpec "Task1" spec
+
+getStringWithError :: Int -> Int -> String
+getStringWithError s count = getString s count ++ "err"
+
+getStringByNumber :: Int -> Int -> String
+getStringByNumber = getString
+
+getString :: Int -> Int -> String
+getString s count
+  | count == 0 = ""
+  | otherwise  = show s ++ ' ' : getString s (count-1)
 
 spec :: Spec
 spec = do
@@ -17,3 +29,7 @@ spec = do
       stringSum "  1   -1"    `shouldBe` Just 0
       stringSum " 1  a -1"    `shouldBe` Nothing
       stringSum " 1r   r-1"   `shouldBe` Nothing
+  describe "property" $
+    prop "stringSum" $ \number count -> do
+      stringSum (getStringByNumber number (abs count + 1))  `shouldBe` Just (number * (abs count + 1))
+      stringSum (getStringWithError number (abs count + 1)) `shouldBe` Nothing

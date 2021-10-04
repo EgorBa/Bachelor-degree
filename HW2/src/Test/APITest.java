b0f69172a6bfe370adcd14e3a772b73d0fbb2d40
EgorBa <tujh.bazhenov.kbn00@mail.ru>
2021-10-04 10:29:31 +0300

    rename

--- HW2/src/Test/APITest.java
+++ HW2/src/Test/APITest.java
@@ -18,26 +18,26 @@ public class APITest {
     final String RESPONSE = "response";
     final String HASHTAG = "hashtag";
     APIUtils APIUtils;
-    NetworkUtils networkUtils;
+    NetworkUtils stubNetworkUtils;
     int[] expectedArray = {0, 1, 2, 3, 4, 5};
 
     @Before
     public void prepare() throws MalformedURLException {
-        networkUtils = Mockito.mock(NetworkUtils.class);
+        stubNetworkUtils = Mockito.mock(NetworkUtils.class);
         for (int i = 0; i < expectedArray.length; i++) {
             URL url = new URL("https://test.ru" + i);
-            Mockito.when(networkUtils.generatedURL(HASHTAG, i)).thenReturn(url);
-            Mockito.when(networkUtils.getResponseFromURL(url)).thenReturn(RESPONSE + i);
-            Mockito.when(networkUtils.getCountFromResponse(RESPONSE + i)).thenReturn(i);
+            Mockito.when(stubNetworkUtils.generatedURL(HASHTAG, i)).thenReturn(url);
+            Mockito.when(stubNetworkUtils.getResponseFromURL(url)).thenReturn(RESPONSE + i);
+            Mockito.when(stubNetworkUtils.getCountFromResponse(RESPONSE + i)).thenReturn(i);
         }
-        APIUtils = new APIUtilsVK(networkUtils);
+        APIUtils = new APIUtilsVK(stubNetworkUtils);
     }
 
     @Test
     public void testGetArray() {
         int[] array = APIUtils.getHoursArray(expectedArray.length, HASHTAG);
-        Mockito.verify(networkUtils, Mockito.times(expectedArray.length)).getCountFromResponse(Mockito.anyString());
-        Mockito.verify(networkUtils, Mockito.times(expectedArray.length)).generatedURL(Mockito.anyString(), Mockito.anyInt());
+        Mockito.verify(stubNetworkUtils, Mockito.times(expectedArray.length)).getCountFromResponse(Mockito.anyString());
+        Mockito.verify(stubNetworkUtils, Mockito.times(expectedArray.length)).generatedURL(Mockito.anyString(), Mockito.anyInt());
         assertEquals(Arrays.toString(array), Arrays.toString(expectedArray));
     }
 
